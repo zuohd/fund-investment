3c801c64fda2092b2d19a0a4ee7cd7d2ac9128c1
Soderberg Zuo <zuo.houde@gmail.com>
2020-04-17 23:08:49 +0800

    check point

--- Soderberg.py
+++ Soderberg.py
@@ -1,6 +1,6 @@
-file_name = 'data/pi_digits.txt'
-with open(file_name) as file_object:
-    # contents = file_object.read()
-    # print(contents.rstrip())
-    for line in file_object:
-        print(line)
+import json
+index_temperature = input("指数温度多少?")
+file_name = "data/records.json"
+with open(file_name,'w+') as f_obj:
+    json.dump(index_temperature,f_obj)
+    print("应该投资{}".format(float(100)))
