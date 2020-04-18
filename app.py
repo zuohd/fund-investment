86230de701d03335de6342a9c2c20fff4763cc28
Soderberg Zuo <zuo.houde@gmail.com>
2020-04-18 22:56:04 +0800

    Fix issues

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import json
 
+
+from fund import FundRecord
+
 index_temperature = input("请输入当前指数温度:")
+fund_record=FundRecord(396)
+fund_record.calculate_reference_amount(float(index_temperature))
 file_name = "data/records.json"
 with open(file_name,'a') as f_obj:
     json.dump(index_temperature,f_obj)
-    print("应该投资{}".format(float(100)))
+    print("应该投资{}".format(fund_record.reference_amount))
