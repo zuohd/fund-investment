e022e74d316f072921e32beeaea850eb495ee32b
Soderberg Zuo <zuo.houde@gmail.com>
2020-04-18 19:51:23 +0800

    Add fund class

---
+++ fund.py
@@ -0,0 +1,13 @@
+import datetime
+
+
+class FundRecord:
+    def __int__(self, index_temperature):
+        self.invest_amount = 396
+        self.create_data = datetime.date.today()
+        self.fund_name = '中证红利'
+        self.fund_code = '10032'
+        self.index_temperature = index_temperature
+        self.left_amount = 0
+        self.reference_amount = 0
+        self.month_amount = 0
